7e7cb96f8bed013346004d234da003073ff68cb5
Mohit Agnihotri <mohit.agnihotri@signify.com>
2022-05-19 14:54:37 +0200

    Implemented data structure

--- chat_message.h
+++ chat_message.h
@@ -1,8 +1,46 @@
-//
-// Created by 310165137 on 18/05/2022.
-//
 
 #ifndef SUPER_INVENTION_CHAT__CHAT_MESSAGE_H_
 #define SUPER_INVENTION_CHAT__CHAT_MESSAGE_H_
+
+
+#define MAX_CLIENT_ID 255
+#define MAX_MESSAGE_LEN 1024
+
+typedef struct chat_message {
+  char version;
+  char type;
+  short message_id;
+  unsigned int payload_length;
+  char payload[0];
+} chat_message_t;
+
+typedef enum message_type {
+  LOGIN = 0x01,
+  MESSAGE = 0x02,
+  ACK = 0x03,
+  NAK = 0x04,
+  LOGOUT = 0x05
+} message_type_t;
+
+typedef struct string_payload
+{
+  unsigned short length;
+  char message[MAX_MESSAGE_LEN];
+}string_payload_t;
+
+typedef struct payload_login
+{
+  char client_id[MAX_CLIENT_ID];
+}payload_login_t;
+
+typedef struct payload_logout
+{
+  char client_id[MAX_CLIENT_ID];
+}payload_logout_t;
+
+typedef struct payload_message {
+  char client_id[MAX_CLIENT_ID];
+  string_payload_t message[MAX_MESSAGE_LEN];
+} payload_message_t;
 
 #endif //SUPER_INVENTION_CHAT__CHAT_MESSAGE_H_
---
+++ user_interface.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "user_interface.h"
+
+
+void get_line_from_stdin(char *line, int max_length)
+{
+  int i = 0;
+  char c;
+  while (i < max_length - 1 && (c = getchar()) != '\n')
+  {
+    line[i] = c;
+    i++;
+  }
+  line[i] = '\0';
+}
+
+
+
+void print_user_menu ( void )
+{
+  printf("Welcome to the Chat client\n");
+  printf("Please select one of the following options:\n");
+  printf("1. Login\n");
+  printf("2. Logout\n");
+  printf("3. Send\n");
+  printf("4. Receive\n");
+}
+
+int get_user_choice( void)
+{
+  char choice;
+  int choice_int;
+  printf("Please enter your choice: ");
+  scanf("%c", &choice);
+  choice_int = atoi(&choice);
+  (void) getc(stdin); //flush \n
+  return choice_int;
+}
