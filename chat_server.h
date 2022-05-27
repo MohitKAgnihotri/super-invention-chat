5b4ba2bd7b90b11e0813ffd5bbf2d875c5facc3e
Mohit Agnihotri <mohit.agnihotri@signify.com>
2022-05-27 16:44:45 +0200

    Fixed code

--- chat_server.h
+++ chat_server.h
@@ -5,15 +5,8 @@
 
 /* Thread routine to serve connection to client. */
 void process_client_message(int server_socket_fd, struct sockaddr_in *client_address, chat_message_t *message);
-
-/* Signal handler to handle SIGTERM and SIGINT signals. */
-void signal_handler(int signal_number);
-
 void send_ack(int socket, struct sockaddr_in *client_address, short message_id);
 void send_nack(int socket, struct sockaddr_in *client_address, short message_id);
-
-void SetupSignalHandler();
-
 int CreateServerSocket(int port);
 
 #endif //SUPER_INVENTION_CHAT__CHAT_SERVER_H_
